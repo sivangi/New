4c2a1061016a9c805427ee9308c4127a72cf7fdd
sivangi <sivangi.sharma@hotmail.com>
2016-10-29 00:21:45 -0700

    Add files via upload

---
+++ WriteBackwards.java
@@ -0,0 +1,21 @@
+package diy;
+
+public class WriteBackwards {
+
+	public static void revMe() {
+
+		String str[] = "Java is easy".split(" ");
+		String revWords = "  ";
+		for (int i = str.length - 1; i >= 0; i--) {
+			revWords += str[i] + "  ";
+
+		}
+		System.out.println(revWords);
+	}
+
+	public static void main(String[] args) {
+
+		WriteBackwards.revMe();
+
+	}
+}
